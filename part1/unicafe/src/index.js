81574e3a21a2f4737ffcd4ea4a977a5392da5227
zyoung1024 <yangzaizxc@qq.com>
2020-07-02 15:54:34 +0800

    part1d exercise upload

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,33 +1,52 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-// Handling arrays
+// The first of web development web:
+// Keep the browser's developer console open at all times
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>button press history:{props.allClicks.join(' ')}</div>
+  )
+
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = (props) => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [allClicks, setAllClicks] = useState([])
+
 
   const handleLeftClick = () => {
-    // 向数组中添加新元素通过concat实现
-    // 该方法不改变现有数组,而是返回新数组的副本
-    setAll(allClicks.concat('L'))
     setLeft(left + 1)
+    setAllClicks(allClicks.concat('L'))
   }
+
   const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    // setAll(allClicks.push('R')) ERROR! React 组件的状态不能直接修改
     setRight(right + 1)
+    setAllClicks(allClicks.concat('R'))
   }
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-        <p>{allClicks.join('')}</p>
-      </div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
